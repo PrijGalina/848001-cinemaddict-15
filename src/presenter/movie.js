ee4f31f7d52a2df897670cce9d08ab36d73fc754
PrijGalina <g_prij@mail.ru>
2021-08-20 17:24:12 +0300

    step 1

---
+++ src/presenter/movie.js
@@ -0,0 +1,35 @@
+import MovieCardView from "../view/movie-view";
+
+export default class MoviesList{
+  constructor(movieContainer) {
+
+  }
+
+  init(movieCard) {
+
+  }
+
+  _renderSort() {
+
+  }
+
+  _renderMovie() {
+    //отрисует 1 фильм
+  }
+
+  _renderMovies() {
+    //вызовывает renderMovie и отрисует весь список
+  }
+
+  _renderNoMovies() {
+
+  }
+
+  _renderLoadMoreButton() {
+
+  }
+
+  _renderMoviesList(){
+    //главный метод, вызывающий все остальные
+  }
+}
